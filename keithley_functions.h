3fdb22abc263441eb2b35bbb1d4e79b13838947b
Nathanael Farley <nasfarley88@gmail.com>
2012-11-27 20:52:49 +0000

    Pulled these from development....
    I'm not sure how that worked. At all.

---
+++ keithley_functions.h
@@ -0,0 +1,17 @@
+#ifndef KEITHLEY_FUNCTIONS
+#define KEITHLEY_FUNCTIONS
+
+using namespace std;
+
+class KeithleyDevice {
+public:
+	void pulsesweepvoltage(double,double,int);
+	void rampvoltagedown(int,int);
+	KeithleyDevice();
+private:
+ 
+	int Device;                   /* Device unit descriptor                  */
+	int BoardIndex;               /* Interface Index (GPIB0=0,GPIB1=1,etc.)  */
+	char stringinput[512]; 
+};
+#endif
