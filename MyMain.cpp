24d9315f42f222afee6c74c43c50c8b275d36917
Nathanael Farley <nasfarley88@gmail.com>
2012-11-27 16:23:23 +0000

    changing Keithley functions into a class

--- MyMain.cpp
+++ MyMain.cpp
@@ -18,6 +18,7 @@
 #include <iostream>
 #include <fstream>
 #include "windows.h"
+#include "keithley_functions.h"
 
 // Trying to update to latest GTK
 #include <glib.h>
@@ -60,6 +61,7 @@ int Device = 0;                   /* Device unit descriptor                  */
 int BoardIndex = 0;               /* Interface Index (GPIB0=0,GPIB1=1,etc.)  */
 static char stringinput[512]; // TODO remove global variables in time for expansion
 
+KeithleyDevice kdevice=KeithleyDevice();
 
 /*-- This function gets the time --*/
 char *gettime()
@@ -82,6 +84,7 @@ gint destroyapp (GtkWidget *widget, gpointer gdata)
   return (FALSE);
 }
 
+<<<<<<< Updated upstream
 static int pulsesweepvoltage(double bottom, double top, int no_of_steps) {
 	// Function to sweep voltage with pulses
 	cout << "Activated pulse voltage" << endl;
@@ -133,6 +136,59 @@ static void rampvoltagedown(int start, int end)
 	}
 
 }
+=======
+//static void pulsesweepvoltage(double bottom, double top, int no_of_steps) {
+//	// Function to sweep voltage with pulses
+//	cout << "Activated pulse voltage" << endl;
+//	cout << "This function sweeps with a pulsing voltage" << endl;
+//	cout << "Lowest voltage " << bottom << " V" << endl;
+//	cout << "Highest voltage " << top << " V" << endl;
+//	cout << "Number of steps " << no_of_steps << endl;
+//
+//	for(int i=0; i<=no_of_steps; i++) {
+//		// Set up the voltage
+//		double tempvolt = bottom + (double)i * ( (top - bottom)/((double)no_of_steps) );
+//
+//		char tempbuff[100];
+//		cout << "votage to set = " << tempvolt << endl;
+//		strcpy(stringinput,":SOUR:VOLT:LEV:AMPL ");
+//		strcat(stringinput,itoa(tempvolt,tempbuff,10));
+//		printf("command: %s\n",stringinput);
+//		ibwrt(Device,stringinput, strlen(stringinput));     /* Send the identification query command   */
+//		Sleep(10);
+//		// TODO Send the output to a file HERE
+//		strcpy(stringinput,":SOUR:VOLT:LEV:AMPL 0");
+//		ibwrt(Device, stringinput, strlen(stringinput));
+//		Sleep(100);
+//
+//		cout << "Pulsed " << tempvolt << " V." << endl;
+//	}
+//
+//	cout << "End of function";
+//
+//	// Should be pretty useful to exit the function with an error code at some point.
+//	//return 0;
+//
+//}
+//static void rampvoltagedown(int start, int end)
+//{
+//	printf("IN RAMP DOWN\n");
+//	printf("Start Voltage %i\n",start);
+//	printf("End Voltage %i\n",end);
+//
+//	for(int i=start; i>end-1; i=i-1)
+//	{
+//		char tempbuff[100];
+//		cout << "votage to set = " << i << endl;
+//		strcpy(stringinput,":SOUR:VOLT:LEV:AMPL "); // fill the first bit of the string with input
+//		strcat(stringinput,itoa(i,tempbuff,10)); // add a bit to the end http://www.cplusplus.com/reference/cstdlib/itoa/
+//		printf("command: %s\n",stringinput); // print the command sent
+//		ibwrt(Device,stringinput, strlen(stringinput));     /* Send the identification query command   */
+//		Sleep(100);
+//	}
+//
+//}
+>>>>>>> Stashed changes
 
 static void rampvoltageup(int start, int end)
 {
@@ -222,7 +278,7 @@ static void button_clicked2()
 	if(startpoint != 0)
 	{
 		printf("Need to ramp down\n");
-		rampvoltagedown(startpoint,endpoint);
+		kdevice.rampvoltagedown(startpoint,endpoint);
 	}
 
 	strcpy(stringinput,":OUTP OFF");
@@ -241,7 +297,7 @@ static void button_clicked4() {
 	// Function for the 'test' button
 	// Mapped to the pulsevoltagesweep function
 
-	pulsesweepvoltage(0,10,100);
+	kdevice.pulsesweepvoltage(0,10,100);
 
 	cout << "button 4 pressed, and function (hopefully) executed.";
 }
@@ -277,7 +333,7 @@ static void setvoltage()
 	}
 	else
 	{
-		rampvoltagedown(startpoint,endpoint);
+		kdevice.rampvoltagedown(startpoint,endpoint);
 	}
 
 	strcpy(stringinput,":SOUR:VOLT:IMM:AMPL?");
@@ -437,15 +493,15 @@ int main(int argc, char* argv[])
    printf("%s\n", Buffer);        /* Print the device identification         */
 
 
-   ibwrt(Device, "*RST", 5);     /* Send the identification query command   */
-   ibwrt(Device, "*CLS", 5);     /* Send the identification query command   */
-   ibwrt(Device, ":SOUR:FUNC VOLT", 15);     /* Send the identification query command   */
-   ibwrt(Device, ":SOUR:VOLT:MODE FIXED", 21);     /* Send the identification query command   */
-   ibwrt(Device, ":SOUR:VOLT:LEV 0", 16);     /* Send the identification query command   */
-   ibwrt(Device, ":SENS:CURR:PROT 150E-6", 22);     /* Send the identification query command   */
-   ibwrt(Device, ":SENS:FUNC 'CURR'", 17);     /* Send the identification query command   */
-   ibwrt(Device, ":FORM:ELEM CURR", 15);     /* Send the identification query command   */
-   ibwrt(Device, ":TRIG:COUN 1", 12);     /* Send the identification query command   */
+   ibwrt(Device, "*RST", 5);     
+   ibwrt(Device, "*CLS", 5);     
+   ibwrt(Device, ":SOUR:FUNC VOLT", 15);     
+   ibwrt(Device, ":SOUR:VOLT:MODE FIXED", 21);     
+   ibwrt(Device, ":SOUR:VOLT:LEV 0", 16);
+   ibwrt(Device, ":SENS:CURR:PROT 150E-6", 22);
+   ibwrt(Device, ":SENS:FUNC 'CURR'", 17);
+   ibwrt(Device, ":FORM:ELEM CURR", 15);
+   ibwrt(Device, ":TRIG:COUN 1", 12);
 
 
 
---
+++ keithley_functions.cxx
@@ -0,0 +1,65 @@
+#include "GPIB.h"
+#include <iostream>
+#include <ctime>
+#include <time.h>
+#include <iomanip>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include "windows.h"
+#include "keithley_functions.h"
+
+KeithleyDevice::KeithleyDevice() {
+	Device = 0;
+	BoardIndex = 0;
+}
+
+void KeithleyDevice::pulsesweepvoltage(double bottom, double top, int no_of_steps) {
+	// Function to sweep voltage with pulses
+	cout << "Activated pulse voltage" << endl;
+	cout << "This function sweeps with a pulsing voltage" << endl;
+	cout << "Lowest voltage " << bottom << " V" << endl;
+	cout << "Highest voltage " << top << " V" << endl;
+	cout << "Number of steps " << no_of_steps << endl;
+
+	for(int i=0; i<=no_of_steps; i++) {
+		// Set up the voltage
+		double tempvolt = bottom + (double)i * ( (top - bottom)/((double)no_of_steps) );
+
+		char tempbuff[100];
+		cout << "votage to set = " << tempvolt << endl;
+		strcpy(stringinput,":SOUR:VOLT:LEV:AMPL ");
+		strcat(stringinput,itoa(tempvolt,tempbuff,10));
+		printf("command: %s\n",stringinput);
+		ibwrt(this->Device,stringinput, strlen(stringinput));     /* Send the identification query command   */
+		Sleep(1);
+		// TODO Send the output to a file HERE
+		strcpy(stringinput,":SOUR:VOLT:LEV:AMPL 0");
+		ibwrt(Device, stringinput, strlen(stringinput));
+		Sleep(10);
+
+		cout << "Pulsed " << tempvolt << " V." << endl;
+	}
+
+	cout << "End of function";
+
+	// Should be pretty useful to exit the function with an error code at some point.
+	//return 0;
+}
+
+void KeithleyDevice::rampvoltagedown(int start, int end) {
+	printf("IN RAMP DOWN\n");
+	printf("Start Voltage %i\n",start);
+	printf("End Voltage %i\n",end);
+
+	for(int i=start; i>end-1; i=i-1)
+	{
+		char tempbuff[100];
+		cout << "votage to set = " << i << endl;
+		strcpy(stringinput,":SOUR:VOLT:LEV:AMPL "); // fill the first bit of the string with input
+		strcat(stringinput,itoa(i,tempbuff,10)); // add a bit to the end http://www.cplusplus.com/reference/cstdlib/itoa/
+		printf("command: %s\n",stringinput); // print the command sent
+		ibwrt(Device,stringinput, strlen(stringinput));     /* Send the identification query command   */
+		Sleep(100);
+	}
+}
