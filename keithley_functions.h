dda099d8d5b1e9272f0696975da0017372f6c1f1
Nathanael Farley <nasfarley88@gmail.com>
2012-11-27 21:01:19 +0000

    fixing merge from development

--- keithley_functions.h
+++ keithley_functions.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef KEITHLEY_FUNCTIONS
 #define KEITHLEY_FUNCTIONS
 
@@ -15,22 +14,4 @@ private:
 	int BoardIndex;               /* Interface Index (GPIB0=0,GPIB1=1,etc.)  */
 	char stringinput[512]; 
 };
-=======
-#ifndef KEITHLEY_FUNCTIONS
-#define KEITHLEY_FUNCTIONS
-
-using namespace std;
-
-class KeithleyDevice {
-public:
-	void pulsesweepvoltage(double,double,int);
-	void rampvoltagedown(int,int);
-	KeithleyDevice();
-private:
- 
-	int Device;                   /* Device unit descriptor                  */
-	int BoardIndex;               /* Interface Index (GPIB0=0,GPIB1=1,etc.)  */
-	char stringinput[512]; 
-};
->>>>>>> contributor/development
 #endif
